1b286f6ec04edb24fb2d0d99e1f5eba0581b556f
Harlan Colclough <harlan.colclough@gmail.com>
2021-11-22 20:07:07 -0800

    switched to google api, need to implement our own search box calling their api

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Constants
-const ubcCenter = [ -123.2473569860864, 49.26216764154615 ];
-const ubcBbox = [ -123.26891308491284, 49.23774036527002, -123.22728520385245, 49.29269032166297 ];
+const ubcCenter = { lat: 49.26216764154615, lng: -123.2473569860864 };
+const ubcBbox = { latLngBounds: { north: 49.28269032166297, south: 49.24774036527002, west: -123.25891308491284, east: -123.23728520385245 },
+    strictBounds: false };
 const startZoom = 14.5;
 
 // Objects
@@ -20,33 +21,19 @@ function openTab(_, tabName) {
     }
 
     if (state.selectedTab === tabName && tabName === 'map') {
-        map.easeTo({center: ubcCenter, zoom: startZoom, bearing: 0, pitch: 0});
+        map.panTo(ubcCenter);
     }
 
     state.selectedTab = tabName;
 }
 
-window.onload = () => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoibWFyYXNvbGVuIiwiYSI6ImNrd2JjeThwMjRid2cyd3BhbTA5M3ZxeGkifQ.3CoiVRdIn5mjproeHoDuwQ';
-
-    map = new mapboxgl.Map({
-        container: 'map-container',
-        style: 'mapbox://styles/mapbox/streets-v11',
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
         center: ubcCenter,
         zoom: startZoom,
-        pitchWithRotate: false,
-        maxBounds: ubcBbox
+        restriction: ubcBbox,
+        disableDefaultUI: true
     });
-
-    map.addControl(
-        new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken,
-            mapboxgl: mapboxgl,
-            bbox: ubcBbox,
-            fuzzyMatch: true,
-            routing: true
-        })
-    );
 
     state = {
         selectedTab: 'map',
