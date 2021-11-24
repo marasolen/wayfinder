87feb4da884a7f93e25294463fd460d17389292a
Mui Tanprasert <43124958+muitanprasert@users.noreply.github.com>
2021-11-23 17:11:46 -0800

    GPS and obstructions

--- main.js
+++ main.js
@@ -3,12 +3,24 @@ const ubcCenter = { lat: 49.26216764154615, lng: -123.2473569860864 };
 const ubcBbox = { latLngBounds: { north: 49.30, south: 49.22, west: -123.27, east: -123.22 },
     strictBounds: false };
 const startZoom = 14.5;
+// Locations
+const icics = { lat: 49.261294, lng: -123.24897 };
+const ikb = { lat: 49.26779158761914, lng: -123.2524405937126 };
+const ams = { lat: 49.26638091855852, lng: -123.24968238815106 };
+const obstructions = [
+    { lat: 49.26322419204356, lng: -123.24936131961685 },
+    { lat: 49.266425700849965, lng: -123.24711426032077 },
+    { lat: 49.26454864720791, lng: -123.2554512122981 },
+    { lat: 49.27214915250998, lng: -123.234117813479 },
+    { lat: 49.252371958011054, lng: -123.24577727156205}
+]
 
 // Objects
 let map;
 let places;
 let autocomplete;
 let pref;
+let marker;
 
 // State
 let state;
@@ -41,6 +53,14 @@ function openTab(_, tabName) {
         }
 
         state.selectedTab = tabName;
+
+         // if opening map, check whether to turn on self-marker
+        if (tabName == 'map'){
+            if (document.getElementById('gps').checked == false)
+                marker.setVisible(false);
+            else
+                marker.setVisible(true);
+        }
     }
 }
 
@@ -76,13 +96,45 @@ function initMap() {
         zoom: startZoom,
         restriction: ubcBbox,
         disableDefaultUI: true,
-        rotateControl: true
+        rotateControl: true,
     });
 
     places = new google.maps.places.PlacesService(map);
 
     autocomplete = new google.maps.places.AutocompleteService();
     document.getElementById('pac-input').onchange = requestAutocomplete;
+
+    marker = new google.maps.Marker({
+        position: icics,
+        map: map,
+        icon: {
+            path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+            scale: 15,
+            fillOpacity: 1,
+            strokeWeight: 5,
+            fillColor: '#0390fc',
+            strokeColor: '#ffffff',
+        },
+    });
+
+    initObstructions();
+}
+
+function initObstructions() {
+    for (var i = 0; i < obstructions.length; i++) {
+        new google.maps.Marker({
+            position: obstructions[i],
+            map: map,
+            icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                scale: 12,
+                fillOpacity: 1,
+                strokeWeight: 2,
+                fillColor: '#fc0303',
+                strokeColor: '#ffffff',
+            },
+        });
+    }
 }
 
 function initPage() {
